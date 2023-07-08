763e03d0a036711dfff0a9cccd49333e4261577b
Tim Galvin <gal16b@petrichor-i1.cm.hpc>
2023-07-08 19:46:19 +1000

    updated MS/check_col_in_ms

--- flint/ms.py
+++ flint/ms.py
@@ -1,8 +1,12 @@
 """Operations related to measurement sets
 """
-from __future__ import annotations
+from __future__ import (
+    annotations,
+)  # Used for mypy/pylance to like the return type of MS.with_options
+
+from os import PathLike
 from pathlib import Path
-from typing import NamedTuple, Optional
+from typing import NamedTuple, Optional, Union
 
 from casacore.tables import table
 
@@ -30,14 +34,20 @@ class MS(NamedTuple):
 # - delete column/rename column
 
 
-def check_column_in_ms(ms: MS, column: Optional[str] = None) -> bool:
+def check_column_in_ms(
+    ms: Union[MS, str, PathLike],
+    column: Optional[str] = None,
+    sub_table: Optional[str] = None,
+) -> bool:
     """Checks to see whether a column exists in an MS. If `column` is provided this
     is checked. It `column` is None, then the MS.column is specified. If both are
     `None` then an error is raised.
 
     Args:
-        ms (MS): The measurement set to check
+        ms (Union[MS, str, PathLike]): The measurement set to check. Will attempt to cast to Path.
         column (Optional[str], optional): The column to check for. Defaults to None.
+        sub_table (Optional[str], optional): A sub-table of the measurement set to inspect. If `None`
+        the main table is examined. Defaults to None.
 
     Raises:
         ValueError: Raised when both `column` and `ms.column` are None.
@@ -45,20 +55,22 @@ def check_column_in_ms(ms: MS, column: Optional[str] = None) -> bool:
     Returns:
         bool: Whether the column exists in the measurement set.
     """
-    # TODO: Support sub-tables
 
-    logger.debug(f"{ms.column=} {column=}")
-    check_col = column if column is not None else ms.column
+    check_col = column
+    if isinstance(ms, MS):
+        logger.debug(f"{ms.column=} {column=}")
+        check_col = column if column is not None else ms.column
+
     if check_col is None:
-        raise ValueError(
-            f"No column to check specified: {ms.path=} {ms.column=} {column=}"
-        )
+        raise ValueError(f"No column to check specified: {ms} {column=}.")
 
-    logger.debug(f"Checking for {check_col}")
-    with table(str(ms.path), readonly=True) as tab:
+    ms_path = ms.path if isinstance(ms, MS) else Path(ms)
+    check_table = str(ms_path) if sub_table is None else f"{str(ms_path)}/{sub_table}"
+
+    logger.debug(f"Checking for {check_col} in {check_table}")
+    with table(check_table, readonly=True) as tab:
         tab_cols = tab.colnames()
-        logger.debug(f"{ms.path} contains columns={tab_cols}.")
-        if check_col in tab_cols:
-            return True
+        logger.debug(f"{ms_path} contains columns={tab_cols}.")
+        result = check_col in tab_cols
 
-        return False
+    return result
