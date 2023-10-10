030aa886e41622e81a2b141ec09128413dc028cb
tgalvin <tim.galvin@csiro.au>
2023-10-10 17:01:22 +0800

    initial toying around of potatopeel

---
+++ flint/peel/potato.py
@@ -0,0 +1,109 @@
+"""Utilities the connect to Stefan Duchesne potatopeel module, which is responsible for
+peeling sources in racs. The repository is available at:
+
+https://gitlab.com/Sunmish/potato/-/tree/main
+
+Potato stands for "Peel Out That Annoying Terrible Object". 
+
+Although this is a python module, for the moment it is expected
+to be in a singularity container. There are several reasons 
+for this, but the principal one is that the numba module used
+by potatopeel may be difficult to get working correctly alongside
+dask and flint. Keeping it simple at this point is the main aim. 
+"""
+
+from argparse import ArgumentParser
+from pathlib import Path
+
+from casacore.tables import table
+
+from flint.logging import logger
+from flint.ms import MS
+
+
+def move_column_into_data(ms: MS) -> MS:
+    """The potatopeel software requires the data column being operated against to be
+    called DATA. This is a requirement of CASA and its gaincal / applysolution task.
+
+    Args:
+        ms (MS): The measurement set that will be edited
+
+    Raises:
+        ValueError: Raised when a column name is expected but can not be found.
+
+    Returns:
+        MS: measurement set with the data column moved into the correct location
+    """
+    data_column = ms.column
+
+    # If the data column already exists and is the nominated column, then we should
+    # just return, ya scally-wag
+    if data_column == "DATA":
+        logger.info(f"{data_column=} is already DATA. No need to rename. ")
+        return ms
+
+    with table(str(ms.path), ack=False, readonly=False) as tab:
+        colnames = tab.colnames()
+        if data_column not in colnames:
+            raise ValueError(
+                f"Column {data_column} not found in {str(ms.path)}. Columns found: {colnames}"
+            )
+
+        # In order to rename the data_column to DATA, we need to make sure that
+        # there is not an existing DATA column
+        if "DATA" in colnames:
+            logger.info("Removing the existing DATA column")
+            tab.deletecol("DATA")
+
+    return ms
+
+
+def potato_peel(ms: MS, potato_container: Path) -> MS:
+    potato_container = potato_container.absolute()
+
+    logger.info(f"Will attempt to peel the {ms=}")
+    logger.info(f"Using the potato peel container {potato_container}")
+
+    # TODO: Need to ensure that the data_column column is moved into DATA
+    # TODO: Should we ensure that there is not CORRECT_DATA before running potatopeel?
+
+    return ms
+
+
+def get_parser() -> ArgumentParser:
+    parser = ArgumentParser(
+        description="A simple interface into the potatopeel software"
+    )
+
+    parser.add_argument(
+        "ms", type=Path, help="Path the to the measurement set with a source to peel"
+    )
+
+    parser.add_argument(
+        "--potato-container",
+        type=Path,
+        default=Path("./potato.sif"),
+        help="Path to the singularity container that represented potatopeel",
+    )
+    parser.add_argument(
+        "--data-column",
+        type=str,
+        default="DATA",
+        help="The column name that contains data with a source that needs to be peeled",
+    )
+
+    return parser
+
+
+def cli():
+    parser = get_parser()
+
+    args = parser.parse_args()
+
+    ms = MS(path=args.ms, column=args.data_column)
+
+    potato_peel(ms=ms, potato_peel=args.potato_container)
+
+
+if __name__ == "__main__":
+    cli()
