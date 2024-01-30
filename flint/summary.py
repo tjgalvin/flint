f28dbcafef8d262781bbf8b6a7360815ec96e964
tgalvin <tim.galvin@csiro.au>
2024-01-30 16:36:17 +0800

    initial summary and tests

--- flint/summary.py
+++ flint/summary.py
@@ -5,48 +5,131 @@ from __future__ import (  # Used for mypy/pylance to like the return type of MS.
 )
 
 from pathlib import Path
-from typing import NamedTuple, Optional
+from typing import NamedTuple, Optional, Union
+
+import numpy as np
+import astropy.units as u
+from astropy.coordinates import AltAz, EarthLocation, SkyCoord, Longitude, Latitude
+from astropy.table import Table
+from astropy.time import Time
 
 from flint.logging import logger
+from flint.ms import MS, get_telescope_location_from_ms, get_times_from_ms
+from flint.naming import get_sbid_from_path
+from flint.source_finding.aegean import AegeanOutputs
+from flint.validation import get_rms_image_info
 
 
 class FieldSummary(NamedTuple):
-    """The main information about a processed field.
+    """The main information about a processed field. This structure is
+    intended to store critical components that might be accumulated throughout
+    processing of a pipeline, and may be most useful when data-products
+    are zipped (or removed) throughout.
 
     There are known issues around serialising astropy units within a dask/prefect environment,
-    for example: https://github.com/astropy/astropy/issues/11317, For this reason these
-    attributes will mostly be primative types, or structures that can
-    be trusted to be serialisable.
+    for example: https://github.com/astropy/astropy/issues/11317, Ye be warned.
     """
 
     sbid: str
     """SBID of processed field"""
     cal_sbid: str
     """SBID of the bandpass calibrator"""
-    radec_j2000_str: str
-    """The RA and Dec (J2000) of the field centre as a string"""
-    gal_lb_str: str
-    """The Galactic coordinates of the field centre as a string"""
-    start_time_str: str = ""
-    """The start time of the observation"""
-    integration_time_str: str = ""
-    """The amount of integration time of the observation (seconds)"""
-    hour_angle_range_str: str = ""
-    """Range in the hour angles of the observation"""
+    centre: Optional[SkyCoord] = None
+    """Centre of the field, which is calculated from the centre of an image"""
+    integration_time: Optional[int] = None
+    """The integration time of the observation (seconds)"""
     median_rms_ujy: Optional[float] = None
     """The median rms of the field in uJy"""
     no_components: Optional[int] = None
     """Number of components found from the source finder"""
     holography_path: Optional[Path] = None
     """Path to the file used for holography"""
+    selfcal_round: Optional[int] = None
+    """The self-cal round"""
+    location: Optional[EarthLocation] = None
+    """The location of the telescope stored as (X,Y,Z) in meters"""
+    ms_times: Optional[Time] = None
+    """The unique scan times of integrations stored in the measurement set"""
+    hour_angles: Optional[Longitude] = None
+    """Computed hour-angles of the field"""
+    elevations: Optional[Latitude] = None
+    """Computed elevations of the field"""
+    median_rms: Optional[float] = None
+    """The meanian RMS computed from an RMS image"""
 
-    def with_options(self, **kwwargs) -> FieldSummary:
-        pass
+    def with_options(self, **kwargs) -> FieldSummary:
+        prop = self._asdict()
+        prop.update(**kwargs)
+
+        return FieldSummary(**prop)
 
 
-def create_field_summary() -> FieldSummary:
-    pass
+def add_rms_information(
+    field_summary: FieldSummary, aegean_outputs: AegeanOutputs
+) -> FieldSummary:
+    rms_image_path = aegean_outputs.rms
+
+    rms_info = get_rms_image_info(rms_path=rms_image_path)
+    centre = rms_info.centre.icrs
+    telescope = field_summary.location
+    ms_times = field_summary.ms_times
+    centre_altaz = centre.transform_to(AltAz(obstime=ms_times, location=telescope))
+    hour_angles = centre_altaz.az.to(u.hourangle)
+    elevations = centre_altaz.alt.to(u.deg)
+
+    no_components = len(Table.read(aegean_outputs.comp))
+
+    field_summary = field_summary.with_options(
+        centre=centre,
+        hour_angles=hour_angles,
+        elevations=elevations,
+        no_components=no_components,
+        median_rms=rms_info.median,
+    )
+
+    return field_summary
+
+
+def create_field_summary(
+    ms: Union[MS, Path],
+    cal_sbid_path: Optional[Path] = None,
+    holography_path: Optional[Path] = None,
+    aegean_outputs: Optional[AegeanOutputs] = None,
+) -> FieldSummary:
+    logger.info("Creating field summary object")
+
+    ms = MS.cast(ms=ms)
+
+    sbid = str(get_sbid_from_path(path=ms.path))
+    cal_sbid = str(get_sbid_from_path(path=cal_sbid_path)) if cal_sbid_path else None
+
+    ms_times = get_times_from_ms(ms=ms)
+    integration = ms_times.ptp().to(u.second).value
+    location = get_telescope_location_from_ms(ms=ms)
+
+    field_summary = FieldSummary(
+        sbid=sbid,
+        cal_sbid=cal_sbid,
+        location=location,
+        integration_time=integration,
+        holography_path=holography_path,
+        ms_times=Time([ms_times.min(), ms_times.max()]),
+    )
+
+    if aegean_outputs:
+        field_summary = add_rms_information(
+            field_summary=field_summary, aegean_outputs=aegean_outputs
+        )
+
+    return field_summary
 
 
 def update_field_summary() -> FieldSummary:
     pass
+
+
+# ms_times = get_times_from_ms(ms=ms)
+#     telescope = get_telescope_location_from_ms(ms=ms)
+#     centre_altaz = centre.transform_to(AltAz(obstime=ms_times, location=telescope))
+#     hour_angles = centre_altaz.az.to(u.hourangle)
+#     elevations = centre_altaz.alt.to(u.deg)
---
+++ tests/test_summary.py
@@ -0,0 +1,117 @@
+import pytest
+import shutil
+from pathlib import Path
+
+import pkg_resources
+import numpy as np
+from astropy.time import Time
+from astropy.coordinates import EarthLocation
+
+from flint.ms import get_times_from_ms, get_telescope_location_from_ms
+from flint.summary import FieldSummary, create_field_summary, add_rms_information
+from flint.source_finding.aegean import AegeanOutputs
+
+
+@pytest.fixture
+def aegean_outputs_example():
+    rms = Path(
+        pkg_resources.resource_filename(
+            "flint", "data/tests/SB39400.RACS_0635-31.beam0-MFS-subimage_rms.fits"
+        )
+    )
+
+    comp = Path(
+        pkg_resources.resource_filename(
+            "flint", "data/tests/SB38959.RACS_1357-18.noselfcal.linmos_comp.fits"
+        )
+    )
+
+    aegean_outputs = AegeanOutputs(bkg=rms, rms=rms, comp=comp)
+
+    return aegean_outputs
+
+
+@pytest.fixture
+def ms_example(tmpdir):
+    ms_zip = Path(
+        pkg_resources.resource_filename(
+            "flint", "data/tests/SB39400.RACS_0635-31.beam0.small.ms.zip"
+        )
+    )
+    outpath = Path(tmpdir) / "39400"
+
+    shutil.unpack_archive(ms_zip, outpath)
+
+    ms_path = Path(outpath) / "SB39400.RACS_0635-31.beam0.small.ms"
+
+    return ms_path
+
+
+def test_field_summary(ms_example):
+    sbid_path = ms_example
+    cal_sbid_path = Path("/scratch3/gal16b/split/39433/SB39433.1934-638.beam0.ms")
+
+    field_summary = create_field_summary(ms=sbid_path, cal_sbid_path=cal_sbid_path)
+
+    assert isinstance(field_summary, FieldSummary)
+    assert field_summary.sbid == "39400"
+    assert field_summary.cal_sbid == "39433"
+
+    field_summary = create_field_summary(ms=sbid_path)
+
+    assert isinstance(field_summary, FieldSummary)
+    assert field_summary.sbid == "39400"
+    assert field_summary.cal_sbid is None
+    assert isinstance(field_summary.location, EarthLocation)
+    assert field_summary.integration_time == 19.90655994415036
+    assert isinstance(field_summary.ms_times, Time)
+    assert len(field_summary.ms_times) == 2
+
+
+def test_field_summary_rms_info(ms_example, aegean_outputs_example):
+    sbid_path = ms_example
+    cal_sbid_path = Path("/scratch3/gal16b/split/39433/SB39433.1934-638.beam0.ms")
+
+    field_summary = create_field_summary(ms=sbid_path, cal_sbid_path=cal_sbid_path)
+
+    assert isinstance(field_summary, FieldSummary)
+    assert field_summary.sbid == "39400"
+    assert field_summary.cal_sbid == "39433"
+    assert isinstance(field_summary.location, EarthLocation)
+    assert field_summary.integration_time == 19.90655994415036
+    assert isinstance(field_summary.ms_times, Time)
+
+    field_summary = add_rms_information(
+        field_summary=field_summary, aegean_outputs=aegean_outputs_example
+    )
+    assert field_summary.no_components == 7225
+    assert np.isclose(field_summary.median_rms, 0.0001515522)
+
+
+def test_field_summary_rms_info_creation(ms_example, aegean_outputs_example):
+    sbid_path = ms_example
+    cal_sbid_path = Path("/scratch3/gal16b/split/39433/SB39433.1934-638.beam0.ms")
+
+    field_summary = create_field_summary(
+        ms=sbid_path, cal_sbid_path=cal_sbid_path, aegean_outputs=aegean_outputs_example
+    )
+
+    assert isinstance(field_summary, FieldSummary)
+    assert field_summary.sbid == "39400"
+    assert field_summary.cal_sbid == "39433"
+    assert isinstance(field_summary.location, EarthLocation)
+    assert field_summary.integration_time == 19.90655994415036
+    assert isinstance(field_summary.ms_times, Time)
+
+
+def test_ms_example_times(ms_example):
+    times = get_times_from_ms(ms=ms_example)
+
+    assert isinstance(times, Time)
+    assert len(times) == 1998
+
+
+def test_ms_example_telescope(ms_example):
+    telescope = get_telescope_location_from_ms(ms=ms_example)
+
+    assert isinstance(telescope, EarthLocation)
