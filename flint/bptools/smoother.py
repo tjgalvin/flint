2093d97783126e65a82d6f5d5150d444ffa8139d
Tim Galvin <gal16b@petrichor-i3.cm.cluster>
2024-02-16 19:51:05 +1100

    added a more correct bandpass phase reference function

--- flint/bptools/smoother.py
+++ flint/bptools/smoother.py
@@ -7,6 +7,48 @@ from scipy.signal import savgol_filter
 from flint.logging import logger
 
 
+def divide_bandpass_by_ref_ant_preserve_phase(complex_gains: np.ndarray, ref_ant: int) -> np.ndarray:
+
+    assert (
+        len(complex_gains.shape) == 3
+    ), f"The shape of the input complex gains should be of rank 3 in form (ant, chan, pol). Received {complex_gains.shape}"
+
+    logger.info(f"Dividing bandpass gain solutions using reference antenna={ref_ant}, using correct phasor")
+
+
+    # Unpack the valuse for short hand use
+    g_x = complex_gains[:,:,0]
+    d_xy = complex_gains[:,:,1]
+    d_yx = complex_gains[:,:,2]
+    g_y = complex_gains[:,:,3]
+
+    # In the operations below our ship only wants to be touching 
+    # the phases in a piratey manner. The amplitudes should remina
+    # unchanged. Construct phasors of the nominated reference antenna
+    ref_g_x = complex_gains[ref_ant, :, 0]
+    ref_g_x = ref_g_x / np.abs(ref_g_x)
+
+    ref_g_y = complex_gains[ref_ant, :, 3]
+    ref_g_y = ref_g_y / np.abs(ref_g_y)
+
+    # Now here is the math, from one Captain Daniel Mitchell
+    # g_x and g_y.d_yx by g_x(ref) and g_y and g_x.d_xy by g_y(ref). i.e. assuming that xy-phase = 0 (due to the ODC) and that the cross terms are leakage
+    g_x_prime = g_x / ref_g_x
+    d_xy_prime = (g_y * d_xy) / ref_g_x
+    d_yx_prime = (g_x * d_yx) / ref_g_y
+    g_y_prime = g_y / ref_g_y
+
+    # Construct the output array to slice things into
+    bp_p = np.zeros_like(complex_gains) * np.nan
+
+    # Place things into place
+    bp_p[:, :, 0] = g_x_prime
+    bp_p[:, :, 1] = d_xy_prime
+    bp_p[:, :, 2] = d_yx_prime
+    bp_p[:, :, 3] = g_y_prime
+
+    return bp_p
+
 def divide_bandpass_by_ref_ant(complex_gains: np.ndarray, ref_ant: int) -> np.ndarray:
     """Divide the bandpass compelx gains (solved for initially by something like
     calibrate) by a nominated reference antenna. In the case of ``calibrate``
@@ -16,7 +58,9 @@ def divide_bandpass_by_ref_ant(complex_gains: np.ndarray, ref_ant: int) -> np.nd
     >> (ant, channel, pol)
 
     Internally this function will construct a phasor:
-    >> phasor = G_{ref_ant} / abs(G_{ref_ant})
+    >> phasor = Jones_{ref_ant} / abs(Jones_{ref_ant})
+    >> shift = Jones_{ref_ant}[0] / and(Jones_{ref_ant}[0])
+    >> phasor_shifted = phasor / shift
 
     which is applied to all antennas in ``complex_gains``.
 
@@ -31,7 +75,7 @@ def divide_bandpass_by_ref_ant(complex_gains: np.ndarray, ref_ant: int) -> np.nd
         len(complex_gains.shape) == 3
     ), f"The shape of the input complex gains should be of rank 3 in form (ant, chan, pol). Received {complex_gains.shape}"
 
-    logger.info(f"Dividing bandpass gain solutions using reference antenna={ref_ant}")
+    logger.info(f"Dividing bandpass gain solutions using reference antenna={ref_ant} with shifted phasor")
 
     # Make a copy of the data to avoid editing it whereever else it might be.
     # Trust nothing you pirate.
@@ -42,9 +86,12 @@ def divide_bandpass_by_ref_ant(complex_gains: np.ndarray, ref_ant: int) -> np.nd
     ref_ant_phasor = ref_ant_solutions / np.abs(ref_ant_solutions)
     ref_ant_shift = (ref_ant_solutions[:, 0] / np.abs(ref_ant_solutions[:, 0]))[:, None]
 
+    # This preserves the consistency of the phase within an single Jones. Essentially
+    # setting the reference to the Xx term of the reference antenna. 
     phasor_shift = ref_ant_phasor / ref_ant_shift
 
-    complex_gains = complex_gains / ref_ant_phasor / phasor_shift[None, :, :]
+    logger.info("Multipply, no divide")
+    complex_gains = complex_gains / ref_ant_phasor[None, :, :] * phasor_shift[None, :, :]
 
     return complex_gains
 
@@ -145,17 +192,13 @@ def smooth_bandpass_complex_gains(
         len(complex_gains.shape) == 3
     ), f"The shape of the input complex gains should be of rank 3 in form (ant, chan, pol). Received {complex_gains.shape}"
 
-    smoothed_complex_gains = np.zeros_like(complex_gains) * np.nan
+    # Duplicate the original, ya filthy pirate
+    smoothed_complex_gains = complex_gains.copy()
 
     ants, chans, pols = complex_gains.shape
 
     logger.info(f"Smoothing using {window_size=} {polynomial_order=}")
-    logger.critical(
-        "BE CAREFUL - there are no constraints to prevent smoothing over spectral window boundaries. "
-    )
-    logger.critical(
-        "Consider whether you really want to be smoothing when using continuum data. "
-    )
+
     for ant in range(ants):
         # TODO: This will be smoothing the X_y and Y_x. Should this actually be done?
         for pol in smooth_jones_elements:
