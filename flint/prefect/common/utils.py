bac6eaa222ff57c16d3b226e06d970f8d8aa6a8b
tgalvin <tim.galvin@csiro.au>
2024-05-24 11:53:42 +0800

    updated string encoding mode

--- flint/prefect/common/utils.py
+++ flint/prefect/common/utils.py
@@ -98,7 +98,7 @@ def task_archive_sbid(
     # or is this fine?
     if max_round:
         updated_file_patterns = tuple(archive_options.tar_file_re_patterns) + (
-            f".*beam[0-9]+\\.round{max_round}-[0-9]{4}-image\\.fits",
+            rf".*beam[0-9]+\.round{max_round}-[0-9]{4}-image\.fits",
         )
         archive_options = archive_options.with_options(
             tar_file_re_patterns=updated_file_patterns
