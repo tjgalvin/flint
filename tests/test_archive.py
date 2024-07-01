abf9bdf2ca0160a34cfa87d17433af0b39334fc9
tgalvin <tim.galvin@csiro.au>
2024-07-01 21:58:49 +0800

    added more test, other non-default options to archive

--- tests/test_archive.py
+++ tests/test_archive.py
@@ -17,6 +17,7 @@ from flint.options import (
     ArchiveOptions,
     DEFAULT_TAR_RE_PATTERNS,
 )
+from flint.utils import get_packaged_resource_path
 
 
 FILES = [f"some_file_{a:02d}-MFS-image.fits" for a in range(36)] + [
@@ -46,6 +47,26 @@ def temp_files(glob_files):
     )
 
     return (base_dir, resolved)
+
+
+@pytest.fixture
+def package_strategy_path():
+    example = get_packaged_resource_path(
+        package="flint", filename="data/tests/test_config.yaml"
+    )
+
+    return example
+
+
+def test_get_archive_options_from_yaml(package_strategy_path):
+    """Get the options from a strategy yaml file for ArchiveOptions"""
+    options = get_archive_options_from_yaml(
+        strategy_yaml_path=Path(package_strategy_path)
+    )
+
+    assert isinstance(options, dict)
+    assert options["tar_file_re_patterns"][-1] == "testing_for_jack.txt"
+    assert options["copy_file_re_patterns"][-1] == "testing_for_sparrow.csv"
 
 
 def test_copy_files_into(tmpdir, temp_files):
