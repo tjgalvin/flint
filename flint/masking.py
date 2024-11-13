03918bc801f10562d61d3f891173dcff759b23b8
tgalvin <tim.galvin@csiro.au>
2024-11-13 13:15:04 +0800

    added adaptive box to seed mbc calculation

--- flint/masking.py
+++ flint/masking.py
@@ -554,6 +554,9 @@ def reverse_negative_flood_fill(
             image=base_image,
             increase_factor=masking_options.flood_fill_positive_seed_clip,
             box_size=masking_options.flood_fill_use_mbc_box_size,
+            adaptive_max_depth=masking_options.flood_fill_use_mbc_adaptive_max_depth,
+            adaptive_box_step=masking_options.flood_fill_use_mbc_adative_step_factor,
+            adaptive_skew_delta=masking_options.flood_fill_use_mbc_adaptive_skew_delta,
         )
         flood_floor_mask = minimum_absolute_clip(
             image=base_image,
