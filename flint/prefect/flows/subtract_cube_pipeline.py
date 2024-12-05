d228bc7361407c3448190fe562b794a941833f13
tgalvin <tim.galvin@csiro.au>
2024-12-05 14:27:52 +0800

    calling coro combine fits

--- flint/prefect/flows/subtract_cube_pipeline.py
+++ flint/prefect/flows/subtract_cube_pipeline.py
@@ -13,9 +13,9 @@ from typing import Tuple, Optional, Union, List
 
 import numpy as np
 from configargparse import ArgumentParser
+from fitscube.combine_fits import combine_fits_coro
 from prefect import flow, unmapped, task
-from fitscube import combine_fits
-from prefect.utilities.asyncutils import run_sync_in_worker_thread
+from prefect.utilities.asyncutils import sync
 
 from flint.coadd.linmos import LinmosCommand
 from flint.configuration import _load_and_copy_strategy
@@ -187,8 +187,8 @@ def task_combine_all_linmos_images(linmos_commands: List[LinmosCommand]) -> Path
     output_cube_path = images_to_combine[0].parent / output_cube_path.name
 
     # combine_fits uses asyncio to accelerate the cube creation
-    _ = run_sync_in_worker_thread(
-        combine_fits,
+    _ = sync(
+        combine_fits_coro,
         file_list=images_to_combine,
         out_cube=output_cube_path,
         max_workers=4,
